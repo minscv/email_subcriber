7e3de8a017d7191fc11217a1e737e8cb9e4621c8
minscv <minscv@outlook.com>
2023-12-20 14:14:30 +0800

    feat: add health_check test

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use std::net::TcpListener;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword
+// We have no .await call, so it is not needed anymore.
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use email_subcriber::run;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+// Bubble up the io::Error if we failed to bind the address
+// Otherwise call .await on our Server
+
+    let listener = TcpListener::bind("1270.0.0.1:0")
+        .expect("Failed to bind random port");
+
+    run(listener)?.await
 }

33a66162bbf2043b93d0470db87c376f1770bf12
minscv <minscv@outlook.com>
2023-12-20 19:07:28 +0800

    feat: add sqlx migration && refactor: new some .rs

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -19,7 +19,7 @@ fi
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
-DB_PORT="${POSTGRES_PORT:=5432}"
+DB_PORT="${POSTGRES_PORT:=5435}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running

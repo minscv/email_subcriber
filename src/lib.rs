f72f4147a040743cf9b64449f241eea4355f82ac
minscv <minscv@outlook.com>
2023-12-20 14:30:43 +0800

    feat: add serder

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
-use std::net::TcpListener;
-use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
@@ -13,8 +19,9 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
     Ok(server)
 }

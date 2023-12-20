9fdf0501ffdccd15be2a2a0fe3f7f1b6dcdf2414
minscv <minscv@outlook.com>
2023-12-20 19:33:08 +0800

    feat: add .env & connect to the database

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
-use email_subcriber::run;
+use email_subcriber::{run, configuration::get_configuration};
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
 // Bubble up the io::Error if we failed to bind the address
 // Otherwise call .await on our Server
 
-    let listener = TcpListener::bind("1270.0.0.1:0")
+    let configuration = get_configuration().expect("Fialed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)
         .expect("Failed to bind random port");
 
     run(listener)?.await
